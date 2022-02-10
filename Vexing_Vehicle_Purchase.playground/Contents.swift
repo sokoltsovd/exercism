ddb26a0770c51f85a84e8cfd1647fddb9e9381bc
DmitrySokoltsov <86969547+dmitrysokoltsov@users.noreply.github.com>
2022-02-10 15:30:21 +0200

    Iteration 1

--- Vexing_Vehicle_Purchase.playground/Contents.swift
+++ Vexing_Vehicle_Purchase.playground/Contents.swift
@@ -5,12 +5,15 @@ import UIKit
  */
 
 func canIBuy(vehicle: String, price: Double, monthlyBudget: Double) -> String {
-  let payment = monthlyBudget * 10
+    let mounthlyPay = price / 60.0
+    let budgetBoundary = 10.0
     
-    if payment > price {
-     return "Yes! I'm getting a \(vehicle)"
-    } else {
+    if mounthlyPay > monthlyBudget + budgetBoundary {
      return "Darn! No \(vehicle) for me"
+    } else if monthlyBudget < mounthlyPay && mounthlyPay <= monthlyBudget + budgetBoundary {
+     return "I'll have to be frugal if I want a \(vehicle)"
+    } else {
+        return "Yes! I'm getting a \(vehicle)"
     }
 }
 
@@ -20,13 +23,36 @@ func canIBuy(vehicle: String, price: Double, monthlyBudget: Double) -> String {
 
 func licenseType(numberOfWheels wheels: Int) -> String {
   switch wheels {
-    case 2...3:
+    case 2,3:
         return "You will need a motorcycle license for your vehicle"
-    case 4...6:
+    case 4,6:
         return "You will need an automobile license for your vehicle"
-    case 8...18:
+    case 18:
         return "You will need a commercial trucking license for your vehicle"
     default:
         return "We do not issue licenses for those types of vehicles"
     }
 }
+
+/*
+ The annual registration fee for your new vehicle is based on the following formula:
+
+ For any vehicle 10 years old or older, the fee is a flat $25.
+ For any newer car:
+ Start with a base cost that is either the Manufacturer's Standard Retail Price (MSRP) for the vehicle, or $25,000 whichever is greater.
+ Then for each year of age, subtract 10% of the base price.
+ Finally, divide that value by 100. Return the nearest integer dollar amount that is less than or equal to this value.
+ */
+
+func registrationFee(msrp: Int, yearsOld: Int) -> Int {
+  var regfee = 0.0;
+  if (yearsOld < 10) {
+     regfee = (((msrp < 25000) ? 25000.0 : Double(msrp)) * (1.0 - Double(yearsOld)/10.0))/100.0;
+  } else {
+     regfee = 25.0
+  }
+  return Int(regfee)
+
+}
+
+
