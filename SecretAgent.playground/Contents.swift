b3497c9e6b79f1f0bd77e5dbf2393eb84926c988
DmitrySokoltsov <86969547+dmitrysokoltsov@users.noreply.github.com>
2022-02-16 09:53:04 +0200

    Iteration 1
    make generateCombination

--- SecretAgent.playground/Contents.swift
+++ SecretAgent.playground/Contents.swift
@@ -28,5 +28,11 @@ func protectSecret(_ secret: String, withPassword password: String) -> (String) 
 
 
 func generateCombination(forRoom room: Int, usingFunction f: (Int) -> Int) -> (Int, Int, Int) {
-  fatalError("Please implement the generateCombination(forRoom:usingFunction) function")
+    let first = f(room)
+    let second = f(first)
+    let third = f(second)
+    
+    return(first, second, third)
 }
+
+
